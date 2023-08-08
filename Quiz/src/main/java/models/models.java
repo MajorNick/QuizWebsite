db24e18529ce4a4a6054954e3c0b997c1a1422b2
Nika Tarkashvili <ntark20@freeuni.edu.ge>
2023-08-08 23:46:12 +0400

    add model classes for notif, announc, ach, user_ach

--- Quiz/src/main/java/models/models.java
+++ Quiz/src/main/java/models/models.java
@@ -2,3 +2,27 @@ package models;
 
 public class models {
 }
+
+public class notifications {
+    public int id;
+    public int receiver_id;
+    public int sender_id;
+    public string notif_type;
+    public string notif_body;
+}
+
+public class announcments {
+    public int id;
+    public string achievement;
+}
+
+public class achievements {
+    public int id;
+    public string achievement;
+}
+
+public class user_achievements {
+    public int id;
+    public int user_id;
+    public int achievement_id;
+}
